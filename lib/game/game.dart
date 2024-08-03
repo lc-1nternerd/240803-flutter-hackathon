5321d020b4cf2bab91847e088505b2c59cba3092
Clemens-Toegel <clemens.toegel@x-tention.at>
2024-08-03 16:39:11 +0200

    FIK-8: place logic

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -2,17 +2,22 @@ import 'dart:async';
 
 import 'package:diefiaker/game/components/combiner_machine.dart';
 import 'package:diefiaker/game/components/conveyor_belt.dart';
-import 'package:diefiaker/game/components/feather_resource.dart';
-import 'package:diefiaker/game/components/iron_resource.dart';
 import 'package:diefiaker/game/components/machine.dart';
 import 'package:diefiaker/game/components/mine.dart';
 import 'package:diefiaker/game/constants.dart';
+import 'package:diefiaker/game/controls/control_model.dart';
 import 'package:diefiaker/game/recipe/recipes.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
+import 'package:flame/image_composition.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
+
+import 'components/resources/feather_resource.dart';
+import 'components/resources/iron_resource.dart';
+import 'components/resources/resource.dart';
 
 class DieFiakerGame extends FlameGame with TapDetector, DragCallbacks {
   DieFiakerGame()
@@ -34,12 +39,145 @@ class DieFiakerGame extends FlameGame with TapDetector, DragCallbacks {
   }
 
   @override
-  void onTapDown(TapDownInfo info) async {
+  void onTapDown(TapDownInfo info) {
     final position = info.eventPosition.widget;
     final gridX = (position.x / tileSize).round() * tileSize;
     final gridY = (position.y / tileSize).round() * tileSize;
     final placePosition = Vector2(gridX, gridY);
 
+    debugPrint('Control Type: ${ControlModel().controlType}');
+
+    switch (ControlModel().controlType) {
+      case ControlType.drag:
+        // TODO: Handle this case.
+        break;
+      case ControlType.mine:
+        placeMineComponent(placePosition: placePosition);
+        break;
+
+      case ControlType.conveyor:
+        placeConveyorBelt(placePosition: placePosition);
+        break;
+
+      case ControlType.machine:
+        placeMachineComponent(placePosition: placePosition);
+        break;
+    }
+  }
+
+  void placeMineComponent({required Vector2 placePosition}) {
+    final components = componentsAtPoint(placePosition);
+
+    if (components.whereType<MineComponent>().isNotEmpty) {
+      debugPrint("wont place another mine if existing");
+      return;
+    }
+
+    final resources = components.whereType<ResourceComponent>();
+
+    if (resources.length == 1) {
+      add(
+        MineComponent(
+          input: resources.first,
+          position: placePosition,
+          height: tileSize,
+          width: tileSize,
+        ),
+      );
+    }
+  }
+
+  void placeMachineComponent({required Vector2 placePosition}) async {
+    // Get machine components on the current position
+    final components =
+        componentsAtPoint(placePosition).whereType<CombinerMachineComponent>();
+
+    // We should at max find one component with type machine component
+    CombinerMachineComponent? existingComponent;
+    if (components.isNotEmpty) {
+      existingComponent = components.first;
+      debugPrint(
+          'Existing component with type: ${existingComponent.runtimeType}');
+    }
+
+    MachineRecipe? selectedRecipe = await showDialog<MachineRecipe?>(
+      context: buildContext!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select machine recipe'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: const Text("Wings"),
+                    onTap: () {
+                      Navigator.of(context).pop(WingRecipe());
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Feet"),
+                    onTap: () {
+                      Navigator.of(context).pop(FootRecipe());
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Body"),
+                    onTap: () {
+                      Navigator.of(context).pop(BodyRecipe());
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Beak"),
+                    onTap: () {
+                      Navigator.of(context).pop(BeakRecipe());
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("Eyes"),
+                    onTap: () {
+                      Navigator.of(context).pop(EyesRecipe());
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(null);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (selectedRecipe != null) {
+      add(
+        CombinerMachineComponent(
+          position: placePosition,
+          recipe: selectedRecipe,
+          inputs: existingComponent?.inputs ?? [],
+        ),
+      );
+    }
+  }
+
+  placeConveyorBelt({required Vector2 placePosition}) {
+    final components = componentsAtPoint(placePosition);
+
+    if (components.whereType<MachineComponent>().isNotEmpty) {
+      debugPrint("wont place a conveyor if existing machine component");
+      return;
+    }
     final offsets = [
       Vector2(-tileSize, 0),
       Vector2(tileSize, 0),
@@ -48,8 +186,7 @@ class DieFiakerGame extends FlameGame with TapDetector, DragCallbacks {
     ];
 
     for (var offset in offsets) {
-      final adjacentComponents =
-          componentsAtPoint(Vector2(gridX, gridY) + offset);
+      final adjacentComponents = componentsAtPoint(placePosition + offset);
 
       if (adjacentComponents.whereType<MineComponent>().isNotEmpty ||
           adjacentComponents.whereType<ConveyorBeltComponent>().isNotEmpty) {
@@ -71,103 +208,10 @@ class DieFiakerGame extends FlameGame with TapDetector, DragCallbacks {
         }
       }
     }
-
-    final components = componentsAtPoint(placePosition);
-
-    MachineRecipe? selectedRecipe;
-    await showDialog<void>(
-        context: buildContext!,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Basic dialog title'),
-            content: Column(
-              children: [
-                const Text("Select the desired recipe for the machine:"),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ListTile(
-                      title: const Text("Wings"),
-                      onTap: () {
-                        selectedRecipe = WingRecipe();
-
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    ListTile(
-                      title: const Text("Feet"),
-                      onTap: () {
-                        selectedRecipe = FootRecipe();
-
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    ListTile(
-                      title: const Text("Body"),
-                      onTap: () {
-                        selectedRecipe = BodyRecipe();
-
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    ListTile(
-                      title: const Text("Beak"),
-                      onTap: () {
-                        selectedRecipe = BeakRecipe();
-
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    ListTile(
-                      title: const Text("Eyes"),
-                      onTap: () {
-                        selectedRecipe = EyesRecipe();
-
-                        Navigator.of(context).pop();
-                      },
-                    )
-                  ],
-                )
-              ],
-            ),
-            actions: <Widget>[
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child: const Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        });
-
-    if (selectedRecipe != null) {
-      add(CombinerMachineComponent(
-          position: placePosition, recipe: selectedRecipe!));
-    }
-
-    // if (components.whereType<MineComponent>().isNotEmpty) {
-    //   print("wont place another mine if existing");
-    //   return;
-    // }
-
-    // final resources = components.whereType<ResourceComponent>();
-
-    // if (resources.length == 1) {
-    //   add(MineComponent(
-    //     input: resources.first,
-    //     position: placePosition,
-    //     height: tileSize,
-    //     width: tileSize,
-    //   ));
-    // }
   }
 
-  // @override
-  // onDragUpdate(DragUpdateEvent details) {
-  //   print("dragging ${details.canvasDelta}");
-  // }
+// @override
+// onDragUpdate(DragUpdateEvent details) {
+//   print("dragging ${details.canvasDelta}");
+// }
 }
