eb0e74f9b2f349927593172cb19114515bee84dd
Lucas Schuster <lucas.schuster@lean-coders.at>
2024-08-03 11:08:23 +0200

    add initial game scaffold

---
+++ lib/game/game.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class DieFiakerGame extends FlameGame {
+  @override
+  Color backgroundColor() {
+    return Colors.amber;
+  }
+}
